d92c39046d034a9737ba619e5ffa13f7fafe6878
Piham ANADA <piham606@gmail.com>
2025-10-05 12:15:30 +0000

    Time: 234 ms (8.51%), Space: 14.7 MB (28.37%) - LeetHub

---
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countConsistentStrings(self, allowed, words):
+        """
+        :type allowed: str
+        :type words: List[str]
+        :rtype: int
+        """
+        count = 0
+        for word in words:
+            if set(word) <= set(allowed):
+                count += 1
+        return count
